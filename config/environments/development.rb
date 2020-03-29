06fec3c5d37bf1d07af77226a4a152335277cb0e
nfcurti <nicolascurti75@gmail.com>
2020-03-29 01:20:45 +0100

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
+config.webpacker.check_yarn_integrity = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 4001 }
 
 end
