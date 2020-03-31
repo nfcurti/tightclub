9a9817451081024a8985ce40cd4813411afd0992
nfcurti <nicolascurti75@gmail.com>
2020-03-31 18:13:21 +0200

    v1.01

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+config.assets.compile = true #set to false by default
+config.assets.digest = true 
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -88,7 +89,18 @@ Rails.application.configure do
 config.webpacker.check_yarn_integrity = false
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+config.mailer_sender = "nicolascurti75@gmail.com"
+  config.action_mailer.default_url_options = { host: "localhost", port: 4001 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => 'nicolascurti75@gmail.com',
+ :password             => '38069123',
+ :authentication       => "plain",
+  }
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_30_213057) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "orders", force: :cascade do |t|
     t.string "client"
     t.string "item"
